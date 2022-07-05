89869d8d4cff40b4307fd80ad5d21e958e995f94
Divsayd <107132042+Divsayd@users.noreply.github.com>
2022-07-05 13:19:01 -0400

    Add files via upload

---
+++ Website1/src/main/java/com/example/demo/model/Contact.java
@@ -0,0 +1,69 @@
+package com.example.demo.model;
+
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class Contact {
+
+    @NotBlank(message="Name should not be blank")
+    @Size(min=2,message="Name must ne atleast 3 characters long")
+    private String name;
+
+    private String mobileNum;
+    private String email;
+    private String subject;
+    private String message;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobileNum() {
+        return mobileNum;
+    }
+
+    public void setMobileNum(String mobileNum) {
+        this.mobileNum = mobileNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "model1{" +
+                "name='" + name + '\'' +
+                ", mobileNum='" + mobileNum + '\'' +
+                ", email='" + email + '\'' +
+                ", subject='" + subject + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
